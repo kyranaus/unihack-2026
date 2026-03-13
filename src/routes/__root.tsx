7df4d511e8c5391db3febcba725ecd98890ffb1c
Liang Chen <liangc2725@gmail.com>
2026-03-13 21:59:52 +1100

    refactor: Replace Header and Footer components with Navbar in RootDocument

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,8 +5,7 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
+import { Navbar } from '../components/Navbar'
 
 import StoreDevtools from '../lib/demo-store-devtools'
 
@@ -57,9 +56,8 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body className="font-sans antialiased [overflow-wrap:anywhere] selection:bg-[rgba(79,184,178,0.24)]">
         <TanStackQueryProvider>
-          <Header />
           {children}
-          <Footer />
+          <Navbar />
           <TanStackDevtools
             config={{
               position: 'bottom-right',
