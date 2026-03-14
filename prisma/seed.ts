e73250af44b0395a794d94623f9813b957ec4efd
Kyran Menezes <kyran@dyn-118-139-61-150.its.monash.edu.au>
2026-03-14 16:56:27 +1100

    changed database url

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '../src/generated/prisma/client.js'
 import { PrismaPg } from '@prisma/adapter-pg'
 
 const adapter = new PrismaPg({
-  connectionString: process.env.LOGIN_DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL!,
 })
 
 const prisma = new PrismaClient({ adapter })
